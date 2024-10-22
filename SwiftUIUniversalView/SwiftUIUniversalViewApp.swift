dee3e952c0520ccd06904235c090b4a571c6771d
JMKim <tdcian71@gmail.com>
2024-10-23 00:01:21 +0900

    push project

---
+++ SwiftUIUniversalView/SwiftUIUniversalViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIUniversalViewApp.swift
+//  SwiftUIUniversalView
+//
+//  Created by 김정민 on 10/22/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIUniversalViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
